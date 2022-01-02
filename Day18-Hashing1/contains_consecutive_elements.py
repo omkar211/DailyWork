9f66c30b84cbc8774d0b41c563ede523bb246d7f
omkar211 <mars64385@gmail.com>
2022-01-03 00:50:32 +0530

    contains_consecutive_elements.py

---
+++ Day18-Hashing1/contains_consecutive_elements.py
@@ -0,0 +1,38 @@
+#Exercise:contains_consecutive_elements
+"""
+Given an unsorted array of size N.if it contains consecutive elements return true.
+"""
+#Bruteforce Approach:
+"""
+1.Sort the array and check A[i]==A[i-1]+1 if not return false else return true.
+"""
+def contains_consecutive_elements(A):
+    A.sort()
+    for i in range(1,len(A)):
+        if A[i]!=(A[i-1]+1):
+            return False
+    return True
+#Time Complexity:O(NlogN)
+#Space Complexity:O(N)
+
+#Optimised Approach
+"""
+1. lets say array is [1,4,3,2,5], If we pick 1 and want to check it consecutive element for   2 in an array.if we find it in O(1). then our complexity will be O(N)
+2. So we will store the elements in dict and pick min element and check in dict and every time increase it by 1 and check it till end.
+"""
+#Code
+def contains_consecutive_elements2(A):
+    dt={}
+    Max=Min=-A[0]
+    for i in range(len(A)):
+        Max=max(Max,A[i])
+        Min=min(Min,A[i])
+        dt[A[i]]=True
+    for i in range(Min,Max+1):
+        if False== dt.get(Min,False):
+            return False
+    return True
+
+#Time Complexity:O(N)
+#Space Complexity:O(1)
+
---
+++ Day18-Hashing1/sub_array_k.py
@@ -0,0 +1,33 @@
+#Exercise: Longest Sub-array sum k
+"""
+Given an unsorted array of length N and K intger ,find out the longest length subarray of sum is equals to K.
+"""
+#Bruteforce
+"""
+1.Find out all the subarrays and check whose sum is equals to k and also checkits length and update it.
+"""
+#Time Complexity:O(N^3)
+#Space Complexity:O(1)
+
+#Optimized Approach
+"""
+lets take an array [x1,x2,x3,x4,x5,x6,x7,x8,x9],
+            lets say y=x1+x2+x3+x4 and k=x5+x6
+            and      prefix_sum = y+k
+            so every time i store y somewhere and  check for y=PS-k if find out then store it and return.        
+"""
+
+#Code
+def sub_array(A,K):
+    dt={}
+    total=0
+    res=-1
+    dt[0]=True
+    for i in range(len(A)):
+        total+=A[i]
+        if dt.get(total-K,False):
+            res=max(res,i-dt[total-K]+1)
+        else:
+            dt[total]=i+1
+    return res
+print(sub_array([6,1,-1,-1,6,2],7))
