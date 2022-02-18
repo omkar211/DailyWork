20f76f4e138b674244980a0d36b56f2e1b0fe0f0
omkar211 <mars64385@gmail.com>
2022-02-18 23:39:15 +0530

    optimized approach is added

--- Day22-Stack/sub_array.py
+++ Day22-Stack/sub_array.py
@@ -20,11 +20,31 @@ def subarray(A):
                 break
     return count+len(A)
 
-#Time Complexity:O(N)
+#Time Complexity:O(N*N)
 #Space Complexity:O(1)
 
 #Optimised Approach
 """
-take an above example,pick 2 adjacent elements where i<j and A[i]>A[j] then no need to consider it in our answer.
-
+1.take an above example,pick 2 adjacent elements where i<j and A[i]>A[j] then no need to consider it in our answer.
+eg:[1,4,2,5,3]
+2.pick first element and insert it in a array and add plus one in resultant and add second element 4 element and calculate the length of array that will give u the number of subarray which have left most element is smallest and add it to the resultant 
+3. pick 3rd element 2. and check it is it greater than last element of the array then add the lenght of array in to the answer but it is smaller . so we remove last element because it voileting our condition ,
+4.after removing it add it to array and add the length of the array in a resultant. and same for all....
 """
+#Code
+def subarray2(A):
+    if len(A)==0:
+        return 0
+    tmp=[]
+    count=1
+    tmp.append(A[0])
+    for i in range(1,len(A)):
+        if A[i]>A[-1]:
+            tmp.append(A[i])
+        else:
+            tmp[-1]=A[i]
+        count+=len(tmp)
+    return tmp
+
+#Time Complexity:O(N)
+#Space Complexity:O(N)
