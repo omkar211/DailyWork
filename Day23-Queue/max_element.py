a6424ab0665cff70daaae88922c5190ccac4a0a6
omkar211c <mars64385@gmail.com>
2022-03-20 00:13:07 +0530

    optimized apprach

--- Day23-Queue/max_element.py
+++ Day23-Queue/max_element.py
@@ -23,4 +23,32 @@ def max_element(A,K):
 #Space Complexity:O(1)
 
 
+#Optimized Appraoch
+"""
+take an above example [2,3,1,10,4,1,2] 
+we will first find the max element from the k elements and store it in a array and put a pointer at index 0 and another pointer at k+1 check if front element is equals to first pointer
+if not it means max element must be a part of next sub array.
+"""
+#Code 
+def find_max_element(A,k):
+    queue=[]
+    max_element=float('inf')
+    for i in range(k):
+        max_element=max(max_element,A[i])
+    queue.append(max_element)
+    print(max_element)
+    first_ptr=0
+    for second_ptr in range(k,len(A)):
+        if queue[0]==first_ptr:
+            queue.remove(0)
+            max_element=A[0]
+        while len(queue)>0:
+            if queue[-1]>A[second_ptr]:
+                queue.append(A[second_ptr])
+                break
+            else:
+                queue.pop()
+        
+#Time Comlexity:O(N)
+#Space Complexity:O(N)
 
