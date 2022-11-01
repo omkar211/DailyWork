2f5b7c7213ad86a48d2ef7b1b88da394b63175e3
dhruv sharma <mars64385@gmail.com>
2022-11-02 01:12:03 +0530

    changes

--- Day25-BST/2_nodes_swapped.py
+++ Day25-BST/2_nodes_swapped.py
@@ -5,19 +5,19 @@ Given BST by mistake 2 nodes get swapped .find out the 2 swapped nodes.
 
 # Approach1:
 """
-So store preorder and make a same copy of preorder and sort it and compare it . 
+So store inorder and make a same copy of inorder and sort it and compare it . 
 point out those elements are not same and that will be our answer.
 """
 # Code
-def preorder(root,lst):
+def In(root,lst):
     if root==None:
         return True
+    inorder(root.left)
     lst.append(root.val)
-    preorder(root.left)
-    preorder(root.right)
+    inorder(root.right)
 
 lst1 = []
-preorder(root,lst1)
+inorder(root,lst1)
 lst2 = list(lst1)
 lst2.sort()
 for i in range(len(lst1)):
@@ -28,19 +28,19 @@ for i in range(len(lst1)):
 
 # Approach 2
 """
-Store an preorder array and count the inversion pairs 
+Store an inorder array and count the inversion pairs 
 form first inversion pair take max value and from second take min.
 but there will be a case where we have only 1 inversion pair.
 Case when both the swapped nodes share an edge.In this case print both the nodes
 """
-def preorder(root,lst):
+def inorder(root,lst):
     if root==None:
         return True
+    inorder(root.left)
     lst.append(root.val)
-    preorder(root.left)
-    preorder(root.right)
+    inorder(root.right)
 lst = []
-preorder(root,lst)
+inorder(root,lst)
 index = 0
 res = []
 for i in range(len(lst)-1):
