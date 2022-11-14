c8641dc84d1a939375ef06ba63ee9cc4e4f19696
dhruv sharma <mars64385@gmail.com>
2022-11-15 02:24:50 +0530

    robot is added

---
+++ DP2/Robot.py
@@ -0,0 +1,14 @@
+# Exercise:
+"""
+Description: ROBOT
+Given a N*M matrix , start and end points
+find the numbers of ways using that from start point, we can reach the end point. 
+"""
+# Approach
+"""
+1. Take 1*1,one way only.
+2. Take 2*2 , ans , only 2 ways.
+3. Take take an example of N*M ,
+  1. We know that the number of ways till matrix[n][m-1]+matrix[n-1][M]
+  
+"""
