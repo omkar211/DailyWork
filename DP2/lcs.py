6f1cc2d0aaffd2aef012ced103b7313ea0280fae
dhruv sharma <mars64385@gmail.com>
2022-11-25 01:43:17 +0530

    tabulation Approach for LCS

--- DP2/lcs.py
+++ DP2/lcs.py
@@ -38,7 +38,34 @@ def lcs(s,t,n,m,dp):
 #Space Compexity:O(N*M)
 
 # Most Optmized Approch(Tabulation Approach or Top-Down Approach)
+# Rules
 """
-
+To solve tabulation problem there are few rules:
+1. Dimension.
+2. Size.
+3. Cell Repeat.
+4. Populate the DP Array.
 """
-    
+# Approach
+"""
+1. We know that,we compare ith element to all the other's string elements.
+2. size of 2-D matrix will be (n+1)*(m+1).
+3. if we pick the ith and jth column of a matrix , it will show the maximum LCS of string from ith and string2 till jth substring .
+4. To get dp[i][j] there are two possibility 
+   1. if s[i]==t[j] then dp[i][j] = 1 + dp[i-1][j-1]
+   2. else dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+"""
+# Code 
+def lcs(s,t):
+    s = " "+s
+    t = " "+t
+    dp = [[0]*(len(t)) for i in range(len(s))]
+    for i in range(1,len(s)):
+        for j in range(1,len(t)):
+            if s[i] == t[j]:
+                dp[i][j] = 1+dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    return dp[i][j]
+#Time Complexity:O(N*M)
+#Space Complexity:O(N*M)
