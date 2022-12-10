7be8a2b6251962e01bbe0d6cf5fe5a4b8fd95264
dhruv sharma <mars64385@gmail.com>
2022-12-10 13:19:10 +0530

    dry state tabulation approach

--- DP3/dry_state.py
+++ DP3/dry_state.py
@@ -60,7 +60,12 @@ def profit(arr,i,j,dp):
 
 # Optimized Approach 2 (Tabulation Approach (Top-Down Approach))
 """
-1.In memoization Approach shell[i][j] is storing the max profit from i to j  substructure.using this information:-
-2. 
-
+1.In memoization Approach shell[i][j] is storing the max profit from i to j substructure.using this information:-
+2. Take an 2d matrix of len(arr)*len(arr)
+3. If fill base for all .After that if we will see before base case values must be zero reason is we can find data index 1 to 4 , we can find 4 to 1 reason is 1st position element will come first , if we choose 1st element .
+4. if we see above reccurence relation we need profit[i+1][j] and profit[i][j-1].
+5. We must proceed diagonally.
 """
+
+# Time Complexity:O(N*N)
+# Space Complexity:O(N*N)
