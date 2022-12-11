119f0226b009dd969eb15b72dec279453d21e631
dhruv sharma <mars64385@gmail.com>
2022-12-11 16:52:45 +0530

    super mario optimized approach

--- DP3/super_mario.py
+++ DP3/super_mario.py
@@ -36,7 +36,34 @@ eg:
 # Time complexity:O(2^(M+N))
 # Space complexity:O(M+N)+stack
 
-# Optmized Approach(memoization Approch)
+# Optmized Approach(Tabulation Approach)
+"""
+1. Take an example
+    [[-2,-3,3],
+    [-5,-10,1],
+    [10,30,-5]]
+2. Shell arr[1][2] will be the part of the path will come through the cell of arr[1][1] and arr[0][1], if i store the min health needed to the next element.So then no need to calculate it again and again.
+3.But if we see form arr[1][1] shell 2 path will goes through it ,So we can store min of both the path.
 """
 
-"""
+def min_health(mat,i,j):
+    n = len(mat)
+    m = len(mat[0])
+    dp = [[0]*m]*n
+    if mat[n-1][m-1]<=0:
+        dp[n-2][m-2] = mat[n-1][m-1]+1
+    else:
+        dp[n-2][m-2] = 1
+    for i in range(n-3,-1,-1):
+        dp[i][m-1] = dp[i+1][m-1]
+    for i in range(m-3,-1,-1):
+        dp[n-1][i] = dp[n-1][i+1]
+    for i in range(n-2,-1,-1):
+        for j in range(m-2,-1,-1):
+            dp[i][j] = min(dp[i-1][j],dp[i][j-1])
+    if dp[0][0]>=0:
+        return 1
+    return -1*dp[0][0]
+
+# Time complexity:O(N*M)
+# Space complexity:O(N*M)
