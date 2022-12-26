53ce63ed156e1d04bef9caa6bcdb11cc01175695
dhruv sharma <mars64385@gmail.com>
2022-12-27 01:11:56 +0530

    gretest subsequece optimized Approach

--- DP4/subsequence_of_T.py
+++ DP4/subsequence_of_T.py
@@ -20,4 +20,13 @@ T = bb
 """
 Like Previous problem create partition at every character and find out LCS of both the string , if we get the answer return True or return False.
 """
+# Time Complexity:O(N*3)
+# Space Complexity:O(N*2)
 
+# Optimized Approach:
+"""
+1. if we create deep copy of S and find LCS with extra condition S1_index!=S2_index.
+2. That will return the gretest subsequence.
+"""
+# Time Complexity:O(N*2)
+# Space Complexity:O(N*2)
